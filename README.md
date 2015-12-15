5337a43252d98ecf20e3d8a0815932bf251ab4cb
[wxb] <[wxb@ipaitao.com]>
2015-12-15 23:21:02 +0800

    创建qrcode项目，第一次提交

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# ChromePlus
-qrcode plus for chrome
+# Qrcode plus for chrome
+
+
+#### qrcode 简介
+* 顾名思义，就是一个chrome的生成二维码的插件，上图
+ ![qrcode](http://d.pcs.baidu.com/thumbnail/c0248aa7c3bc972a56dcabc115a25ca1?fid=3775695419-250528-1010062733208273&time=1450191600&rt=pr&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-MjoT6Bt%2fMr2zxB%2bGZYDhQXfhAJA%3d&expires=8h&chkbd=0&chkv=0&dp-logid=8093351945890682303&dp-callid=0&size=c1440_u900&quality=90)
+* 其实 `google webstore` 有很多类似的插件，为什么还要自己写一个，两个原因
+	* 不能满足我的需求，
+	* 尝试一下chrome 的插件开发，虽然之前已经写过几个插件
+* 与其他插件有什么不同？
+	* 点击二维码图，有高清版，为什么需要高清，因为有时候连接特别长，生成的图密密麻麻的，收集识别起来特别慢，放大能更快的识别
+	* 可以不用跳转页面，在input中输入自定义连接或者文本，及时生成二维码，方便，快捷！
+
+### 欢迎下载试用
+* 百度云盘： http://pan.baidu.com/s/1skyOYfj
+* 有任何问题可以提issue
+* 时间仓促，功能简单，代码简陋，见谅！
+
+### 安装方法
+* chrome 输入地址 `chrome://extensions/`,或者手动设置 -> 更多工具 -> 扩展程序 
+* 把下载的qrcode.crx 托进来，提示安装，就ok了
+
+
+<hr/>
+
+### 后续。。。
+ * 目前我还没有注册`google webstore`的开发账号，无法上传到网上，可以直接下载安装
+ * 后面会陆续开发其他的插件，来更好的提高工作效率。也欢迎大家提意见。
---
+++ qrcode/js/qr.js
@@ -0,0 +1,55 @@
+﻿function creatQrcode(target, url, title, width, width) {
+    jQuery(target).qrcode({
+        text: url,
+        width: width,
+        height: width
+    });
+    $("#qr_webname").text(title);
+    $("#qr_website").text(url);
+
+}
+//提示信息
+function showMsg(msg) {
+    $("#J_msg").html(msg).animate({
+        marginTop: '0px',
+        opacity: '1'
+    });
+    setTimeout(function() {
+        $("#J_msg").animate({
+            marginTop: '-28px',
+            opacity: '0'
+        }).html('');
+    }, 1500);
+}
+jQuery(function() {
+    $("#qr_a").click(function() {
+        chrome.tabs.create({
+            selected: true,
+            url: "http://www.ocsen.wang"
+        });
+    });
+    //点击自动生成
+    $("#J_creatQr").click(function() {
+        var url = $('#J_userUrl').val();
+        if (url !== '') {
+            $('canvas').remove();
+            creatQrcode('#qrDiv', url, '用户自定义网站', '160', '160');
+            creatQrcode('#qrDivHight', url, '用户自定义网站', '320', '320');
+        } else {
+            showMsg('请输入网址！')
+        }
+    });
+
+    $('#qrDiv').click(function() {
+        $('#qrDivHight').show();
+    });
+
+    $('#qrDivHight').click(function() {
+        $(this).hide();
+    });
+});
+
+chrome.tabs.getSelected(null, function(w) {
+    creatQrcode('#qrDiv', w.url, w.title, '160', '160');
+    creatQrcode('#qrDivHight', w.url, w.title, '320', '320');
+});
